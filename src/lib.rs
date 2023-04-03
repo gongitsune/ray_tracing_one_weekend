f7487795b2ac32f8fb8c0b357f1cdc11edc176de
kara <89068705+gongitsune@users.noreply.github.com>
2023-04-04 03:53:52 +0900

    6.2. Simplifying the Ray-Sphere Intersection Code

--- src/lib.rs
+++ src/lib.rs
@@ -24,14 +24,14 @@ fn ray_color(ray: &Ray) -> Color {
 fn hit_sphere(center: &Vec3, radius: f64, ray: &Ray) -> f64 {
     let oc = ray.origin() - center;
     let a = ray.direction().dot(&ray.direction());
-    let b = 2.0 * oc.dot(&ray.direction());
-    let c = oc.dot(&oc) - radius * radius;
-    let discriminant = b * b - 4.0 * a * c;
+    let b = oc.dot(&ray.direction());
+    let c = oc.dot(&oc) - radius.powi(2);
+    let discriminant = b.powi(2) - a * c;
 
     if discriminant < 0.0 {
         -1.0
     } else {
-        (-b - discriminant.sqrt()) / (2.0 * a)
+        (-b - discriminant.sqrt()) / a
     }
 }
 
