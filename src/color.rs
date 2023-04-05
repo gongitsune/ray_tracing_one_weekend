3b13ce89a347d9c706351bf86f583be4ae331c20
kara <89068705+gongitsune@users.noreply.github.com>
2023-04-05 16:38:22 +0900

    7.2. Generating Pixels with Multiple Samples

--- src/color.rs
+++ src/color.rs
@@ -7,14 +7,20 @@ use crate::vec::Color;
 /**
  * Write color
  */
-pub fn write_color<W: Write>(writer: &mut BufWriter<W>, pixel_color: Color) -> Result<()> {
-    let translate = |x: f64| (255.999 * x) as usize;
+pub fn write_color<W: Write>(
+    writer: &mut BufWriter<W>,
+    pixel_color: Color,
+    samples_per_pixel: usize,
+) -> Result<()> {
+    let cast = |x: f64| (256.0 * x.clamp(0.0, 0.999)) as usize;
+
+    let scale = 1.0 / samples_per_pixel as f64;
     writeln!(
         writer,
         "{} {} {}",
-        translate(pixel_color.x),
-        translate(pixel_color.y),
-        translate(pixel_color.z)
+        cast(pixel_color.x * scale),
+        cast(pixel_color.y * scale),
+        cast(pixel_color.z * scale)
     )?;
 
     Ok(())
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+mod camera;
 mod color;
 mod hittable;
 mod ray;
@@ -5,9 +6,11 @@ mod sphere;
 mod vec;
 
 use anyhow::{Ok, Result};
+use camera::Camera;
 use color::write_color;
 use hittable::{Hittable, HittableList};
 use indicatif::ProgressBar;
+use rand::Rng;
 use ray::Ray;
 use sphere::Sphere;
 use std::{
@@ -27,25 +30,14 @@ fn ray_color<H: Hittable>(ray: &Ray, world: &H) -> Color {
     (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
 }
 
-fn hit_sphere(center: &Vec3, radius: f64, ray: &Ray) -> f64 {
-    let oc = ray.origin() - center;
-    let a = ray.direction().dot(&ray.direction());
-    let b = oc.dot(&ray.direction());
-    let c = oc.dot(&oc) - radius.powi(2);
-    let discriminant = b.powi(2) - a * c;
-
-    if discriminant < 0.0 {
-        -1.0
-    } else {
-        (-b - discriminant.sqrt()) / a
-    }
-}
-
 pub fn draw<W: Write>(
     aspect_ratio: f64,
     img_width: usize,
+    samples_per_pixel: usize,
     writer: &mut BufWriter<W>,
 ) -> Result<()> {
+    let mut rng = rand::thread_rng();
+
     // Image
     let img_height = (img_width as f64 / aspect_ratio) as usize;
 
@@ -59,15 +51,7 @@ pub fn draw<W: Write>(
     ]);
 
     // Camera
-    let viewport_height = 2.0;
-    let viewport_width = aspect_ratio * viewport_height;
-    let focal_length = 1.0;
-
-    let origin = Vec3::new(0.0, 0.0, 0.0);
-    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
-    let vertical = Vec3::new(0.0, viewport_height, 0.0);
-    let lower_left_corner =
-        origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
+    let camera = Camera::new(aspect_ratio, 2.0, 1.0, Vec3::new(0.0, 0.0, 0.0));
 
     // Render
     writeln!(writer, "P3\n{} {}\n255", img_width, img_height)?;
@@ -75,15 +59,14 @@ pub fn draw<W: Write>(
     for j in (0..img_height).rev() {
         pb.inc(1);
         for i in 0..img_width {
-            let u = i as f64 / (img_width - 1) as f64;
-            let v = j as f64 / (img_height - 1) as f64;
+            let pixel_color = (0..samples_per_pixel).fold(Vec3::new(0.0, 0.0, 0.0), |color, _| {
+                let u = (i as f64 + rng.gen::<f64>()) / (img_width - 1) as f64;
+                let v = (j as f64 + rng.gen::<f64>()) / (img_height - 1) as f64;
 
-            let ray = Ray::new(
-                origin,
-                lower_left_corner + u * horizontal + v * vertical - origin,
-            );
-            let pixel_color = ray_color(&ray, &world);
-            write_color(writer, pixel_color)?;
+                let ray = camera.get_ray(u, v);
+                color + ray_color(&ray, &world)
+            });
+            write_color(writer, pixel_color, samples_per_pixel)?;
         }
     }
 
