916b75964e97b42ebb41a43ad430f161c7ed6a68
kara <89068705+gongitsune@users.noreply.github.com>
2023-04-05 16:12:39 +0900

    6.5. A List of Hittable Objects

--- src/hittable.rs
+++ src/hittable.rs
@@ -1,4 +1,5 @@
 use crate::{ray::Ray, vec::Vec3};
+use std::sync::Arc;
 
 pub struct HitRecord {
     pub point: Vec3,
@@ -8,4 +9,24 @@ pub struct HitRecord {
 
 pub trait Hittable {
     fn hit(&self, ray: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord>;
+}
+
+pub struct HittableList<H: Hittable> {
+    pub objects: Vec<Arc<H>>,
+}
+
+impl<H: Hittable> Hittable for HittableList<H> {
+    fn hit(&self, ray: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord> {
+        let mut closest_so_far = t_max;
+        let mut hit_anything = None;
+
+        for object in &self.objects {
+            if let Some(hit) = object.hit(ray, t_min, closest_so_far) {
+                closest_so_far = hit.t;
+                hit_anything = Some(hit);
+            }
+        }
+
+        hit_anything
+    }
 }
